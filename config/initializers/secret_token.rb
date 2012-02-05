9afef198f7433cac236c58c812b9f5bc9d8545ee
Etherealite <evanbanghm@yahoo.com>
2012-02-05 15:57:17 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongopractice::Application.config.secret_token = 'f1f178bb2551623acd90ea741e1a38e2bc20f7e47b5fdc70612ca1de402fdcefbcf576ea9f9380775bedd2efd6b3418c5b9af7ddaeff81646a86666e72eef6cd'
